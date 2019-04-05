6737f8cebca5255eee9d369fd51f86e855b8be23
George Apazidis <geoapa@gmail.com>
2019-04-05 12:12:23 -0700

    fix for 100% height on mobile

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-// let vh = window.innerHeight * 0.01;
+let vh = window.innerHeight * 0.01;
 // // Then we set the value in the --vh custom property to the root of the document
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 let questionNum = 0; // used for accessing array index
 let score = 0;
